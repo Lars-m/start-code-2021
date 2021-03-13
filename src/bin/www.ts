69f5e25b88044cc7a8f6a3ab9e2027b064bc1398
unknown <lam@cphbusiness.dk>
2021-03-13 08:50:38 +0100

    minor changes

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -1,8 +1,7 @@
 import app from "../app"
-// import d from 'debug'
-// const debug = d("www");
 const debug = require("debug")("www");
 
 const PORT = process.env.PORT || 3333;
-console.log(PORT)
+
+console.log(process.env.DEBUG)
 app.listen(PORT, () => debug(`Server started, listening on PORT: ${PORT}`))
